11346a5584855319a6ba6233a73bcf82a57063c8
Yoon2442 <yoonhw20@naver.com>
2023-04-12 14:21:51 +0900

    git init

---
+++ src/main/java/com/js/Application.java
@@ -0,0 +1,12 @@
+package com.js; // 없어서 안됐었음 - 2023-04-12
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        // System.out.println("Hello world!");
+    }
+}
